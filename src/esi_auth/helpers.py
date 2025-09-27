93584e34cb40dbad4c82469b8d167e2446acb192
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-27 09:02:14 -0700

    add helper functions for user agent and package metadata retrieval

---
+++ src/esi_auth/helpers.py
@@ -0,0 +1,46 @@
+from importlib import metadata
+
+from .settings import get_settings
+
+
+def get_user_agent() -> str:
+    """Construct the User-Agent string for HTTP requests."""
+    settings = get_settings()
+    user_portion = f"{settings.user_app_name}/{settings.user_app_version} (eve:{settings.character_name}; {settings.user_email})"
+    app_metadata = metadata.metadata("esi-auth")
+    app_name = app_metadata["name"]
+    app_version = app_metadata["version"]
+    app_source_url = get_package_url("esi-auth", "Source")
+    _, author_email = get_author_email("esi-auth")
+    esi_auth_portion = f"{app_name}/{app_version} ({author_email}; +{app_source_url})"
+    return f"{user_portion} {esi_auth_portion}"
+
+
+def get_package_url(package_name: str, url_name: str) -> str:
+    """Get the project URL for a given package."""
+    # TODO add to snippets.
+    try:
+        app_metadata = metadata.metadata(package_name)
+        project_urls = app_metadata.get_all("Project-URL")
+        if project_urls:
+            # Project-URL can contain multiple URLs separated by commas
+            urls = [url.split(",") for url in project_urls]
+            for url in urls:
+                if len(url) == 2 and url[0].strip().lower() == url_name.lower():
+                    return url[1].strip()
+        return "Unknown"
+    except metadata.PackageNotFoundError:
+        return "Unknown"
+
+
+def get_author_email(package_name: str) -> tuple[str, str]:
+    """Get the author email and name from package metadata."""
+    # TODO add to snippets.
+    try:
+        app_metadata = metadata.metadata(package_name)
+        app_author_email = app_metadata.get("Author-email", "Unknown")
+        author_name, author_email = app_author_email.split(" <", 1)
+        author_email = author_email.rstrip(">")
+        return author_name, author_email
+    except metadata.PackageNotFoundError:
+        return "Unknown", "Unknown"

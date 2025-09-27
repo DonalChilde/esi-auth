61c9af67eba84c29840f894c7b0858a64bf37ed2
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-27 16:51:14 -0700

    add validator for json list field from env file

--- src/esi_auth/settings.py
+++ src/esi_auth/settings.py
@@ -1,16 +1,16 @@
+"""Application settings for esi-auth."""
+
+import json
 from collections.abc import Sequence
 from pathlib import Path
 from typing import Any, ClassVar
 
 import typer
-from pydantic import Field
+from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 APPLICATION_NAME = "pfmsoft-esi-auth"
 DEFAULT_APP_DIR = Path(typer.get_app_dir(APPLICATION_NAME))
-
-# TODO load and save to file
-# TODO cli command to update oauth metadata
 
 
 class EsiAuthSettings(BaseSettings):
@@ -77,76 +77,8 @@ class EsiAuthSettings(BaseSettings):
         default="NOT_SET",
         description="EVE Online application client secret from developers.eveonline.com",
     )
-    scopes: Sequence[str] = Field(
-        default=[
-            "esi-corporations.read_projects.v1",
-            "publicData",
-            "esi-calendar.respond_calendar_events.v1",
-            "esi-calendar.read_calendar_events.v1",
-            "esi-location.read_location.v1",
-            "esi-location.read_ship_type.v1",
-            "esi-mail.organize_mail.v1",
-            "esi-mail.read_mail.v1",
-            "esi-mail.send_mail.v1",
-            "esi-skills.read_skills.v1",
-            "esi-skills.read_skillqueue.v1",
-            "esi-wallet.read_character_wallet.v1",
-            "esi-wallet.read_corporation_wallet.v1",
-            "esi-search.search_structures.v1",
-            "esi-clones.read_clones.v1",
-            "esi-characters.read_contacts.v1",
-            "esi-universe.read_structures.v1",
-            "esi-killmails.read_killmails.v1",
-            "esi-corporations.read_corporation_membership.v1",
-            "esi-assets.read_assets.v1",
-            "esi-planets.manage_planets.v1",
-            "esi-fleets.read_fleet.v1",
-            "esi-fleets.write_fleet.v1",
-            "esi-ui.open_window.v1",
-            "esi-ui.write_waypoint.v1",
-            "esi-characters.write_contacts.v1",
-            "esi-fittings.read_fittings.v1",
-            "esi-fittings.write_fittings.v1",
-            "esi-markets.structure_markets.v1",
-            "esi-corporations.read_structures.v1",
-            "esi-characters.read_loyalty.v1",
-            "esi-characters.read_chat_channels.v1",
-            "esi-characters.read_medals.v1",
-            "esi-characters.read_standings.v1",
-            "esi-characters.read_agents_research.v1",
-            "esi-industry.read_character_jobs.v1",
-            "esi-markets.read_character_orders.v1",
-            "esi-characters.read_blueprints.v1",
-            "esi-characters.read_corporation_roles.v1",
-            "esi-location.read_online.v1",
-            "esi-contracts.read_character_contracts.v1",
-            "esi-clones.read_implants.v1",
-            "esi-characters.read_fatigue.v1",
-            "esi-killmails.read_corporation_killmails.v1",
-            "esi-corporations.track_members.v1",
-            "esi-wallet.read_corporation_wallets.v1",
-            "esi-characters.read_notifications.v1",
-            "esi-corporations.read_divisions.v1",
-            "esi-corporations.read_contacts.v1",
-            "esi-assets.read_corporation_assets.v1",
-            "esi-corporations.read_titles.v1",
-            "esi-corporations.read_blueprints.v1",
-            "esi-contracts.read_corporation_contracts.v1",
-            "esi-corporations.read_standings.v1",
-            "esi-corporations.read_starbases.v1",
-            "esi-industry.read_corporation_jobs.v1",
-            "esi-markets.read_corporation_orders.v1",
-            "esi-corporations.read_container_logs.v1",
-            "esi-industry.read_character_mining.v1",
-            "esi-industry.read_corporation_mining.v1",
-            "esi-planets.read_customs_offices.v1",
-            "esi-corporations.read_facilities.v1",
-            "esi-corporations.read_medals.v1",
-            "esi-characters.read_titles.v1",
-            "esi-alliances.read_contacts.v1",
-            "esi-characters.read_fw_stats.v1",
-            "esi-corporations.read_fw_stats.v1",
-        ],
+    scopes: list[str] = Field(
+        default=["publicData"],
         description="Default OAuth2 scopes for authentication",
     )
 
@@ -225,6 +157,19 @@ class EsiAuthSettings(BaseSettings):
         self.app_dir.mkdir(parents=True, exist_ok=True)
         self.log_dir.mkdir(parents=True, exist_ok=True)
         self.token_store_dir.mkdir(parents=True, exist_ok=True)
+
+    @field_validator("scopes", mode="before")
+    @classmethod
+    def json_decode(cls, v: Any) -> list[str]:
+        """Decode JSON string to list if necessary."""
+        if isinstance(v, str):
+            try:
+                return json.loads(v)
+            except ValueError:
+                pass
+        if isinstance(v, list):
+            return v
+        raise ValueError("Invalid format for scopes")
 
 
 class SettingsManager:

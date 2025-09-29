5e1065cb6b1e1cfa93c8835ce0194b95e23aaf50
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-29 08:35:24 -0700

    fix oauth metadata model

--- src/esi_auth/auth_helpers.py
+++ src/esi_auth/auth_helpers.py
@@ -64,14 +64,15 @@ class OauthMetadata(TypedDict):
     issuer: str
     authorization_endpoint: str
     token_endpoint: str
-    revocation_endpoint: str
-    jwks_uri: str
     response_types_supported: list[str]
+    jwks_uri: str
+    revocation_endpoint: str
     subject_types_supported: list[str]
-    id_token_signing_alg_values_supported: list[str]
-    scopes_supported: list[str]
+    revocation_endpoint_auth_methods_supported: list[str]
     token_endpoint_auth_methods_supported: list[str]
-    claims_supported: list[str]
+    id_token_signing_alg_values_supported: list[str]
+    token_endpoint_auth_signing_alg_values_supported: list[str]
+    code_challenge_methods_supported: list[str]
 
 
 class OauthToken(TypedDict):
